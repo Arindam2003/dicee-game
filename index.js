7178504d5dbfe78fc9688d55b73ec3377b8f6a13
Arindam2003 <dindaarindam000@gmail.com>
2024-12-18 00:25:32 +0530

    Dice Game Completed

---
+++ index.js
@@ -0,0 +1,24 @@
+function random(){
+    var randomNumber1=Math.floor(Math.random()*6)+1;
+    var randomdiceimage="dice"+randomNumber1+".png";
+    var imgsource="images/" + randomdiceimage;
+    
+    var image1=document.querySelectorAll("img")[0];
+    image1.setAttribute("src",imgsource);
+
+    var randomNumber2=Math.floor(Math.random()*6)+1;
+    var randomdiceimage2="dice"+randomNumber2+".png";
+    var imgsource2="images/" + randomdiceimage2;
+    
+    var image2=document.querySelectorAll("img")[1];
+    image2.setAttribute("src",imgsource2);
+    
+    if(randomNumber1>randomNumber2)
+    {
+        document.querySelector("h1").innerHTML="Motu Win"
+    }else{
+        document.querySelector("h1").innerHTML="Patlu Win"
+    }
+}
+
+
